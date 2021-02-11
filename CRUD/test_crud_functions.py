bed1740057cc9050692af595e2ec72b0bb883cc0
benjamin-oriol <oriol.benjamin@gmail.com>
2021-02-11 12:25:42 +0100

    test read csv

--- CRUD/test_crud_functions.py
+++ CRUD/test_crud_functions.py
@@ -1,7 +1,7 @@
 from unittest import TestCase
 import crud_functions
 import csv
-from Includes import common
+from Includes import common, data_manager
 
 
 class Test_CRUD(TestCase):
@@ -12,6 +12,7 @@ class Test_CRUD(TestCase):
 
 
 class Test(TestCase):
+
     def test_add(self):
 
         table = [['test 1', 'test 2', 'test 3', 'test 4', 'test 5', 'test 6'],
@@ -71,6 +72,7 @@ class Test(TestCase):
         self.assertEqual(result, expected_result)
 
     def test_id(self):
+
         table = [['test1', 'test 2', 'test 3', 'test 4', 'test 5', 'test 6'],
                  ['1', 'data 2', 'data 3', 'data 4', 'data 5', 'data 6'],
                  ['2', 'data 2', 'data 3', 'data 4', 'data 5', 'data 6'],
@@ -80,5 +82,23 @@ class Test(TestCase):
         result = common.generate_random(table)
 
         expected_result = '5'
+
+        self.assertEqual(result, expected_result)
+
+    def test_read_csv(self):
+
+        filename = "table_test.csv"
+
+        expected_result = [['Libelle', 'Prix', 'Quantitï¿½', 'Montant'],
+                             ['1', 'Audit interne', '23000', '2', '46000'],
+                             ['2', 'Suite Power BI', '400', '4', '16000'],
+                             ['3', 'Formation risques credit', '1300', '3', '3900'],
+                             ['4', 'Audit sï¿½curitï¿½ ', '4000', '1', '40000'],
+                             ['5', 'Loyer annuel', '160000', '1', '160000'],
+                             ['6', 'Provisions', '860000', '1', '860000'],
+                             ['7', 'Personnel', '11500000', '1', '11500000'],
+                             ['8', 'Amortissement', '12000', '1', '120000']]
+
+        result = data_manager.get_table_from_file(filename)
 
         self.assertEqual(result, expected_result)
