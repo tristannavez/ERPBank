e84219943bff28ee60c52894f602e1e3a21445d3
Tristan Navez <trs.nvz@outlook.fr>
2021-02-12 14:01:38 +0100

    Tests ok

--- test_crud_functions.py
+++ test_crud_functions.py
@@ -54,7 +54,7 @@ class Test(unittest.TestCase):
                  ['id5', 'data 2', 'data 3', 'data 4', 'data 5', 'data 6']]
 
         _id = 'id3'
-        test = [_id, 'test 2', 'test 3', 'test 4', 'test 5', 'test 8']
+        test = [_id, 'test 2', 'test 3', 'test 4', 'test 5', 'test 6']
 
         result = crud_functions.update(table, _id, test=test)
 
