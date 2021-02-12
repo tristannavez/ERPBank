ae4cc94ca37162eb9b3a9938cf94398159e39eba
Tristan Navez <trs.nvz@outlook.fr>
2021-02-12 13:28:38 +0100

    pipeline + all tests INTEGRATED

--- test_crud_functions.py
+++ test_crud_functions.py
@@ -82,11 +82,11 @@ class Test(unittest.TestCase):
     def test_read_csv(self):
         filename = "table_test.csv"
 
-        expected_result = [['Libelle', 'Prix', 'Quantitï¿½', 'Montant'],
+        expected_result = [['Libelle', 'Prix', 'Quantite', 'Montant'],
                            ['1', 'Audit interne', '23000', '2', '46000'],
                            ['2', 'Suite Power BI', '400', '4', '16000'],
                            ['3', 'Formation risques credit', '1300', '3', '3900'],
-                           ['4', 'Audit sï¿½curitï¿½ ', '4000', '1', '40000'],
+                           ['4', 'Audit securite', '4000', '1', '40000'],
                            ['5', 'Loyer annuel', '160000', '1', '160000'],
                            ['6', 'Provisions', '860000', '1', '860000'],
                            ['7', 'Personnel', '11500000', '1', '11500000'],
